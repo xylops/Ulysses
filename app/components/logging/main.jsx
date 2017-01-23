5da33c462fbcc03f99e2433731b192072f0260d5
Xylops <rankine1607@gmail.com>
2017-01-24 00:42:55 +0800

    playing around with time

--- app/components/logging/main.jsx
+++ app/components/logging/main.jsx
@@ -1,11 +1,15 @@
 var React = require('react');
 var {connect} = require('react-redux');
-
+var moment = require('moment')
+//material-ui
+import TimePicker from 'material-ui/TimePicker';
+import DatePicker from 'material-ui/DatePicker';
+//api
 var loggingAPI = require('../../api/loggingAPI')
 
 var main = React.createClass ({
+
     componentWillMount:function(){
-        console.log(new Date)
         var options = {
           from: new Date - 24 * 60 * 60 * 1000,
           until: new Date,
@@ -14,12 +18,35 @@ var main = React.createClass ({
           order: 'desc',
         //   fields: ['message']
         };
+        // loggingAPI.queryLog(options)
+    },
+    dateChange:function(e, date){
+        var dateTwo = moment(date).format('YYYYMMDD')
+        var dateThree = moment(dateTwo).format('x')
+
+        console.log(dateThree)
+        var options = {
+          from: dateThree,
+          until: new Date,
+          limit: 5,
+          start: 0,
+          order: 'desc',
+        //   fields: ['message']
+        };
         loggingAPI.queryLog(options)
+    },
+    timeChange:function(result){
+        console.log(this.refs.inputValue.value)
     },
     render:function(){
         return (
             <div>
+                <br/>
                 Logging session
+                <DatePicker hintText="Landscape Dialog" mode="landscape" onChange={this.dateChange}/>
+                <DatePicker hintText="Landscape Dialog" mode="landscape" onChange={this.timeChange}/>
+                <input type="datetime-local" onChange={this.timeChange} ref="inputValue"/>
+
             </div>
         )
     }
